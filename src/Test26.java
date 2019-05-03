1fe96f88a941147b85fbe11812b0f8ab027a13b4
mateng123 <mateng689@163.com>
2019-05-03 17:12:22 +0800

    第二十六题

--- src/Test26.java
+++ src/Test26.java
@@ -1,3 +1,23 @@
 public class Test26 {
-    //TODO
+    TreeNode head = null;
+    TreeNode readHead = null;
+    public TreeNode Convert(TreeNode pRootOfTree) {
+        ConvertHelper(pRootOfTree);
+        return readHead;
+    }
+
+    public void ConvertHelper(TreeNode pRootOfTree){
+        if(pRootOfTree==null){
+            return;
+        }
+        ConvertHelper(pRootOfTree.left);
+        if(head==null){
+            head=readHead=pRootOfTree;
+        }else{
+            head.right = pRootOfTree;
+            pRootOfTree.left = head;
+            head = pRootOfTree;
+        }
+        ConvertHelper(pRootOfTree.right);
+    }
 }
