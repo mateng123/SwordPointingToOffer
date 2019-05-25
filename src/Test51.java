b018bd9d49941098a12d9c2cfc82eb7d5aecccc1
mateng123 <mateng689@163.com>
2019-05-25 13:31:58 +0800

    第五十一题

---
+++ src/Test51.java
@@ -0,0 +1,15 @@
+public class Test51 {
+    public int[] multiply(int[] A) {
+        int[] B = new int[A.length];
+        B[0]=1;
+        for(int i=1;i<A.length;i++){
+            B[i]=B[i-1]*A[i-1];
+        }
+        int temp = 1;
+        for(int i=A.length-2;i>=0;i--){
+            temp *= A[i+1];
+            B[i] *= temp;
+        }
+        return B;
+    }
+}
