0336197f5092cacecf0ecf9d8528adb371007cbb
mateng123 <mateng689@163.com>
2019-05-26 13:06:47 +0800

    第五十六题

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+    public ListNode deleteDuplication(ListNode pHead)
+    {
+        if(pHead==null||pHead.next==null){
+            return pHead;
+        }
+        if(pHead.val == pHead.next.val){
+            ListNode pNode = pHead.next;
+            while(pNode!=null&&pHead.val==pNode.val){
+                pNode = pNode.next;
+            }
+            return deleteDuplication(pNode);
+        }else{
+            pHead.next = deleteDuplication(pHead.next);
+            return pHead;
+        }
+    }
+}
